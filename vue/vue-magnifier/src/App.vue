5d34fc9d1e7c3ebb85c152e734be735cf781e946
kkkokokrunch <320018789@qq.com>
2019-11-19 16:27:22 +0800

    vue实现放大镜组件

---
+++ vue/vue-magnifier/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <image-zoom 
+    :src="src"
+    :bigSrc="bigSrc"></image-zoom>
+  </div>
+</template>
+
+<script>
+import ImageZoom from './components/ImageZoom'
+export default {
+  name: 'App',
+  components: {
+    ImageZoom
+  },
+  data() {
+    return {
+      src:'https://img.alicdn.com/imgextra/i1/1714128138/O1CN01m9gkSC29zFkBWX7um_!!1714128138.jpg_430x430q90.jpg',
+      bigSrc:'https://img.alicdn.com/imgextra/i1/1714128138/O1CN01m9gkSC29zFkBWX7um_!!1714128138.jpg'
+    }
+  },
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
