a117048658373d1b61f621d7f7bf7caad65be32a
kkkokokrunch <320018789@qq.com>
2019-11-21 09:09:41 +0800

    防抖

--- vue/vue-music/src/router/index.js
+++ vue/vue-music/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Search from '@/components/pages/search'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'Search',
+        component: Search
+    }, {
+        path: '/search',
+        name: 'Search',
+        component: Search
+    }]
 })
